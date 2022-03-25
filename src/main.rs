f51ca5a4c449a8199edd8e143f3d3ac034938432
Tim Enders <enders.tim@gmail.com>
2022-03-25 16:02:07 -0500

    Initial Map and Player build

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
 mod map;
+mod player;
 
 mod prelude {
-    use bracket_lib::prelude::*;
+    pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
 
-struct State {}
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            ),
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
