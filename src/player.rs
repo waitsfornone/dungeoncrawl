d51e5c979e920c9e9586ed435d98ecb4bdd36cdd
Tim Enders <enders.tim@gmail.com>
2022-04-01 14:58:37 -0500

    Camera functionality, and out of bounds as walls

--- src/player.rs
+++ src/player.rs
@@ -11,10 +11,11 @@ impl Player {
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm) {
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(1);
         ctx.set(
-            self.position.x,
-            self.position.y,
+            self.position.x - camera.left_x,
+            self.position.y - camera.top_y,
             WHITE,
             BLACK,
             to_cp437('@'),
@@ -22,7 +23,7 @@ impl Player {
     }
 
     // This handles player movement
-    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map, camera: &mut Camera) {
         if let Some(key) = ctx.key {
             let delta = match key {
                 VirtualKeyCode::Left => Point::new(-1, 0),
@@ -35,6 +36,7 @@ impl Player {
             let new_position = self.position + delta;
             if map.can_enter_tile(new_position) {
                 self.position = new_position;
+                camera.on_player_move(new_position);
             }
         }
     }
